455dea170e339744e34a74c849182e41372abe9c
matt <matthew.j.dailey@gmail.com>
2015-03-27 09:00:12 -0700

    Added unit tests

---
+++ recognition-server/src/test/java/co/speechre/MavenBuildTest.java
@@ -0,0 +1,17 @@
+package co.speechre;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MavenBuildTest {
+
+	/**
+	 * Verify that mvn test is running unit tests.
+	 */
+	@Test
+	public void testBuildRunsTest() {
+		assertTrue(true);
+	}
+	
+}
