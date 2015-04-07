86a64c03e841d9cb31f33297b3238a523582fe05
matt <matthew.j.dailey@gmail.com>
2015-04-06 19:36:02 -0700

    Make timeout sleep time a param

--- recognition-server/src/main/java/co/speechre/api/TimeoutTest.java
+++ recognition-server/src/main/java/co/speechre/api/TimeoutTest.java
@@ -2,9 +2,11 @@ package co.speechre.api;
 
 import java.io.IOException;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,13 +16,11 @@ public class TimeoutTest {
 
 	private final static Logger log = LoggerFactory.getLogger(TimeoutTest.class);
 	
-	private final static int TIMEOUT = 10000;
-	
 	@GET 
 	@Produces("application/json")
-	public String get() throws IOException, InterruptedException {
-		log.debug("Received request to timeout test. Sleeping for {} ms.", TIMEOUT);
-		Thread.sleep(TIMEOUT);
+	public String get(@DefaultValue("10000") @QueryParam("timeout") int timeout) throws IOException, InterruptedException {
+		log.debug("Received request to timeout test. Sleeping for {} ms.", timeout);
+		Thread.sleep(timeout);
     	log.debug("Completed request!");
 		return "Timeout complete.";
 	}
