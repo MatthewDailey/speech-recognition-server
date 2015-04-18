694ffff0a44d1f74e749aa4a8c20245b971dee63
Matt Dailey <matthew.j.dailey@gmail.com>
2015-04-18 10:17:28 -0700

    Serialized form to return from RecognizeUploadResource

---
+++ recognition-server/src/main/java/com/saypenis/speech/api/serialization/RoundResultBean.java
@@ -0,0 +1,29 @@
+package com.saypenis.speech.api.serialization;
+
+public class RoundResultBean {
+
+	public final String round_id;
+	public final long date;
+	public final long lat;
+	public final long lon;
+	public final String name;
+	public final double score;
+	public final String uri;
+	public final String user_id;
+	public final String transcription;
+	
+	public RoundResultBean(String round_id, long date, long lat, long lon,
+			String name, double score, String uri, String user_id,
+			String transcription) {
+		this.round_id = round_id;
+		this.date = date;
+		this.lat = lat;
+		this.lon = lon;
+		this.name = name;
+		this.score = score;
+		this.uri = uri;
+		this.user_id = user_id;
+		this.transcription = transcription;
+	}
+	
+}
